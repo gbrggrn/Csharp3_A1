61022e35164b9a30d7dc3ee7f2bb87dc274a43be
gbrggrn <gustberg107@student.kau.se>
2025-09-02 13:44:35 +0200

    Setup

--- obj/Debug/net8.0/Csharp3_A1.AssemblyInfo.cs
+++ obj/Debug/net8.0/Csharp3_A1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Csharp3_A1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2006605c7a0c71e8895520586c8a419ab56c9245")]
 [assembly: System.Reflection.AssemblyProductAttribute("Csharp3_A1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Csharp3_A1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
