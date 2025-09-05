84423dadde1b56c9a4650be1eec7e61d0ae39d21
gbrggrn <gustberg107@student.kau.se>
2025-09-05 08:14:56 +0200

    Nested dropdowns in main menu added

--- obj/Debug/net8.0/Csharp3_A1.AssemblyInfo.cs
+++ obj/Debug/net8.0/Csharp3_A1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Csharp3_A1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f640c49d5039b3d934c374494a73a935c8972f5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94015240bbe0c8c774d66c5c55cf37951d492625")]
 [assembly: System.Reflection.AssemblyProductAttribute("Csharp3_A1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Csharp3_A1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
