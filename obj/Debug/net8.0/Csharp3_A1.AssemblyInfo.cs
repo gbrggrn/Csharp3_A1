ca31ac86b8ce697ff138e7a2824c6e89a17a4816
gbrggrn <gustberg107@student.kau.se>
2025-09-15 16:02:55 +0200

    Changed index header/image + staff dashboard appointments nested sidebar now in working order with content

--- obj/Debug/net8.0/Csharp3_A1.AssemblyInfo.cs
+++ obj/Debug/net8.0/Csharp3_A1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Csharp3_A1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a89df324ebdc24d2dc3a4e79b1bf6fb18fd5dc4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ad7afa5886bdddc847e901d9ef4a9761b047bb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Csharp3_A1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Csharp3_A1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
