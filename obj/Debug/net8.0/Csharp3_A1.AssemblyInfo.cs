e4fdb8af761c5d87c6cf6e0a9c80be27b6fa1ddc
gbrggrn <gustberg107@student.kau.se>
2025-09-18 14:50:27 +0200

    Schedule Appointment button added to staff dashboard

--- obj/Debug/net8.0/Csharp3_A1.AssemblyInfo.cs
+++ obj/Debug/net8.0/Csharp3_A1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Csharp3_A1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca31ac86b8ce697ff138e7a2824c6e89a17a4816")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb451d6e4d69f7b7d35843a7c9da2799f215dd2f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Csharp3_A1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Csharp3_A1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
