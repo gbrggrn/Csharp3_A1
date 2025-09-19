15e017ecc06937eb29fb5d69b0c3199ad94aca76
gbrggrn <gustberg107@student.kau.se>
2025-09-19 16:22:29 +0200

    Patients can now schedule appointments with mock values

--- obj/Debug/net8.0/Csharp3_A1.AssemblyInfo.cs
+++ obj/Debug/net8.0/Csharp3_A1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Csharp3_A1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b81bb0a6c263f7bf03cdf99e490782d8decfad1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2453800518b909eda45f3b6bdb8582a00d52d5b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Csharp3_A1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Csharp3_A1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
