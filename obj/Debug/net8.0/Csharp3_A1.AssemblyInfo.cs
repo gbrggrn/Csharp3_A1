5b4aee0d89c1d2e1719997e36b6eb24cf1bf17ab
gbrggrn <gustberg107@student.kau.se>
2025-09-22 13:40:03 +0200

    A minimal layout extracted to _MinimalLayout which renders the partial _Header and the footer of _Layout. PatientPortal uses _MinimalLayout

--- obj/Debug/net8.0/Csharp3_A1.AssemblyInfo.cs
+++ obj/Debug/net8.0/Csharp3_A1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Csharp3_A1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+643921dbd6c4fb25419e7cbe83666ae75b93fd25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55767800a96e645f6362eaf0a0483b35e20467df")]
 [assembly: System.Reflection.AssemblyProductAttribute("Csharp3_A1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Csharp3_A1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
