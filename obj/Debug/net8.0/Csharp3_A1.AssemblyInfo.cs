e1ad40b9db90431ce32fd5222b9494b14d5b62eb
gbrggrn <gustberg107@student.kau.se>
2025-09-22 13:44:47 +0200

    Auto AssemblyInfo changes

--- obj/Debug/net8.0/Csharp3_A1.AssemblyInfo.cs
+++ obj/Debug/net8.0/Csharp3_A1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Csharp3_A1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15e017ecc06937eb29fb5d69b0c3199ad94aca76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+643921dbd6c4fb25419e7cbe83666ae75b93fd25")]
 [assembly: System.Reflection.AssemblyProductAttribute("Csharp3_A1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Csharp3_A1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
