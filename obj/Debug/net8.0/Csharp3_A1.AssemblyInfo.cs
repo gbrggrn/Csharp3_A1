d21a5fc61baf1bbecf62cc52c771650d5c194810
gbrggrn <gustberg107@student.kau.se>
2025-09-25 19:27:09 +0200

    AutoGen

--- obj/Debug/net8.0/Csharp3_A1.AssemblyInfo.cs
+++ obj/Debug/net8.0/Csharp3_A1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Csharp3_A1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1ad40b9db90431ce32fd5222b9494b14d5b62eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55ed4658c09773841e3591338d7195cf9763acdb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Csharp3_A1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Csharp3_A1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
